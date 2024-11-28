e167a7804ae3cbe3a92851a610073c67d538636a
tim939422 <duosifan@hotmail.com>
2024-11-28 20:40:17 +0800

    ignore python

---
+++ src/m_poisson.f90
@@ -0,0 +1,11 @@
+module m_poisson
+    !> module of FFT acceralated Poisson Solver Class
+    !>
+    !> copyright - Yang Group, BUAA
+    !>
+    !> author - D. Fan, 2024-11-28
+    implicit none
+    
+contains
+    
+end module m_poisson
